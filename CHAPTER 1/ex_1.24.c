8a84c36693bd6445c12c6812cfd4bc2255024f8f
DHARANEESH46 <haridharani46@gmail.com>
2026-01-19 19:49:05 +0530

    A TUTORIAL INTRODUCTION

--- CHAPTER 1/ex_1.24.c
+++ CHAPTER 1/ex_1.24.c
@@ -1,3 +1,7 @@
+/*Write a program to check a C program for rudimentary syntax errors like
+unmatched parentheses, brackets and braces. Don't forget about quotes, both single and
+double, escape sequences, and comments.*/
+
 #include <stdio.h>
 
 #define MAXSTR 10000
